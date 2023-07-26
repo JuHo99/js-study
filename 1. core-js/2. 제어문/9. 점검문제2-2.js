4b853ee40372a84a07065a40a50619873e885afa
JoHo99 <ghdi926@naver.com>
2023-07-26 10:12:31 +0900

    core js updown게임 마무리

--- 1. core-js/2. 제어문/9. 점검문제2-2.js
+++ 1. core-js/2. 제어문/9. 점검문제2-2.js
@@ -1,3 +1,30 @@
+
+// 난이도 상수
+const HIGH = 1;
+const MIDDLE = 2;
+const LOW = 3;
+
+// 초기 난이도 설정 선택창 띄우기
+let level = +prompt('난이도를 선택하세요!# [1. 상(3번의 기회) | 2. 중(6번의 기회) | 3. 하(10번의 기회)]');
+
+// 초기 카운트 수
+let initCount;
+
+
+if (level === HIGH) {
+  initCount = 3;
+} else if (level === MIDDLE) {
+  initCount = 6;
+} else if (level === LOW) {
+  initCount = 10;
+} else {
+  alert('잘못 입력했으니까 난이도 상으로 자동 시작합니다.');
+  initCount = 3;
+}
+
+// 카운트다운 변수
+let countdown = initCount;
+
 // 실제로 정답으로 사용할 랜덤 정수 만들기
 let secret = Math.floor(Math.random() * 100) + 1;
 
@@ -16,10 +43,12 @@ while (true) {
     continue;
   }
 
+  countdown--; // 카운트 감소
+
   // 업 다운 판단
   // 정답인 경우
   if (secret === answer) {
-    alert(`정답입니다!`);
+    alert(`정답입니다! ${initCount - countdown}번만에 맞췄습니다!`); // 게임종료 조건 1
     break;
   } else if (secret > answer) {
     alert(`UP!!`);
@@ -28,6 +57,15 @@ while (true) {
     alert(`DOWN!!`);
     max = answer - 1;
   }
-}
+
+  // 추가 게임 종료 조건
+  if (countdown === 0) {
+    alert(`ㅠㅠ 패배하셨습니다! 정답은 ${secret}였지롱 ㅋㅋ`);
+    break;
+  } else {
+    alert(`${countdown}번의 기회가 남았습니다.`);
+  }
+
+} // end while loop
 
 alert(`수고하셨습니다!`);
