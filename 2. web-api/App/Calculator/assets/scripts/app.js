0a53dd67a7b5a25828b80dbe92854d38fb3c8560
JoHo99 <ghdi926@naver.com>
2023-08-02 11:23:32 +0900

    web - api 실습 : 2.리팩토링,로그이력 기능

--- 2. web-api/App/Calculator/assets/scripts/app.js
+++ 2. web-api/App/Calculator/assets/scripts/app.js
@@ -2,65 +2,85 @@
 // index.html에서 활용할 이벤트처리, 핸들러 등등 작성
 
 // 현재 계산기에 표시될 숫자
-let currentResult = 0; 
+let currentResult = 0;
 
+// 사용자의 입력값을 읽어오는 함수 제작
+const getUserNumberInput = () => +$userInput.value;
 
+// 계산 이력을 모아둘 배열 제작
+const logEntries = [];
+
+// 로그 이력을 만드는 함수 제작
+const writeToLog = (operation,prevResult,number,result) =>{
+    // 연삭, 이전결과 , 연산숫자, 연산결과
+    const logObject = {
+        operation,
+        prevResult,
+        number,
+        result
+    };
+    logEntries.push(logObject);
+    console.log(logEntries);
+};
+
+// 계산 로그를 만들고 화면에 렌더링 하는 함수 제작
+const createLogAndRenderOutput = (operteMark,originResult,calcNumber) => {
+    // 로그 생성 (기존의 숫자 + enteredNumber)
+    const calcDescriptionLog = `${originResult} ${operteMark} ${calcNumber}`;
+    // 화면에 렌더링
+    outputResult(currentResult, calcDescriptionLog)
+};
 
 
 //================= 이벤트 핸들러 제작==================
 
 // 더하기 버튼 이벤트 핸들러
-const addHandler = () =>{
-    console.log( '+ button click' );
+const addHandler = () => {
+    console.log('+ button click');
     // 입력창에 입력한 숫자를 읽기
-    const enteredNumber = +$userInput.value;
-    // 로그 생성 (기존의 숫자 + enteredNumber)
-    const calcDescriptionLog = `${currentResult} + ${enteredNumber}`;
+    const enteredNumber = getUserNumberInput();
+    // 계산 전 값을 백업
+    const originResult = currentResult;
     // 실제 결과 반영
     currentResult += enteredNumber;
-    // 화면에 렌더링
-    outputResult(currentResult,calcDescriptionLog)
+    // 로그생성 + 화면에 렌더링 하는 함수 호출
+    createLogAndRenderOutput('+',originResult,enteredNumber);
+    // 로그이력 쌓기
+    writeToLog('ADD',originResult,enteredNumber,currentResult);
 };
 
 // 빼기 버튼 이벤트 핸들러
-const subtractHandler = ()=>{
+const subtractHandler = () => {
     console.log('- button click');
-
-        const enteredNumber = +$userInput.value;
-
-        const calcDescriptionLog = `${currentResult} - ${enteredNumber}`;
-
-        currentResult -= enteredNumber;
-
-        outputResult(currentResult,calcDescriptionLog)
+    const enteredNumber = getUserNumberInput();
+    const originResult = currentResult;
+    currentResult -= enteredNumber;
+    createLogAndRenderOutput('-',originResult,enteredNumber);
+    writeToLog('SUB',originResult,enteredNumber,currentResult);
 };
 
 // 곱하기 버튼 이벤트 핸들러
-const multiplyHandler = () =>{
+const multiplyHandler = () => {
     console.log('* button click');
-    const enteredNumber = +$userInput.value;
-
-    const calcDescriptionLog = `${currentResult} * ${enteredNumber}`;
-
+    const enteredNumber = getUserNumberInput();
+    const originResult = currentResult;
     currentResult *= enteredNumber;
-    
-    outputResult(currentResult,calcDescriptionLog)
+    createLogAndRenderOutput('×',originResult,enteredNumber);
+    writeToLog('MULT',originResult,enteredNumber,currentResult);
 };
 
 // 나누기 버튼 이벤트 핸들러
-const divideHandler = () =>{
-    console.log( '/ button click' );
-    const enteredNumber = +$userInput.value;
-
-    const calcDescriptionLog = `${currentResult} / ${enteredNumber}`;
-
+const divideHandler = () => {
+    console.log('/ button click');
+    const enteredNumber = getUserNumberInput();
+    const originResult = currentResult;
     currentResult /= enteredNumber;
-    
-    outputResult(currentResult,calcDescriptionLog)
+    createLogAndRenderOutput('÷',originResult,enteredNumber);
+    writeToLog('DIVIDE',originResult,enteredNumber,currentResult);
 };
 
 // ================이벤트 핸들러 바인딩===================
-$addBtn.addEventListener('click',addHandler);
-$subtractBtn.addEventListener('click',subtractHandler);
-$multiplyBtn.addEventListener('click',multiplyHandler);
-$divideBtn.addEventListener('click',divideHandler);
+$addBtn.addEventListener('click', addHandler);
+$subtractBtn.addEventListener('click', subtractHandler);
+$multiplyBtn.addEventListener('click', multiplyHandler);
+$divideBtn.addEventListener('click', divideHandler);
