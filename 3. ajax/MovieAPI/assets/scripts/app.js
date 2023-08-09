acc8fa6e9fc8f3bb31e4f83febf23e2c16159363
JoHo99 <ghdi926@naver.com>
2023-08-09 10:28:38 +0900

    ajax - 실습 DELETE 기능 생성

--- 3. ajax/MovieAPI/assets/scripts/app.js
+++ 3. ajax/MovieAPI/assets/scripts/app.js
@@ -42,13 +42,14 @@ const renderTodos = (todoList) => {
 
 // =========== 이벤트 관련 함수 =========== //
 
+// 생성 버튼 클릭 이벤트
 const addTondoHandler = e =>{
   // 1. 클릭 확인
   // console.log('클릭');
   // 2.클릭시 인풋 텍스트 읽기
   const $textInput = document.getElementById('todo-text');
   console.log($textInput);
-  const inputText = $textInput.ariaValueMax;
+  const inputText = $textInput.value;
   // 3. 데이터를 보내서 서버에 저장해야함 ( fetchTodos )
   // payload를 API스펙에 맞게 만들어서 보내야함
   const payload = {
@@ -66,6 +67,23 @@ const addTondoHandler = e =>{
   })
 };
 
+// 삭제버튼 클릭 이벤트
+const deleteTodoHandler = e => {
+  if (!e.target.matches('.remove span')) return;
+  // 클릭한 할 일을 지우기 위해 id값을 가져와야 함
+  // console.log( (1)e.target (2).closest('.todo-list-item')(3).dataset.id);
+  const id = e.target.closest('.todo-list-item').dataset.id;
+  // console.log(id);
+  fetchTodos(`${URL}/${id}`,'DELETE')
+  .then(res =>{
+    if (res.status ===200) {
+      console.log('삭제 성공');
+    }else{
+      console.log('삭제 실패');
+    }
+  })
+
+}
 
 
 
@@ -77,6 +95,9 @@ const $addBtn = document.getElementById('add');
 $addBtn.addEventListener('click',addTondoHandler)
 
 
+// step3. 할 일 삭제 기능 만들기
+
+$todoList.addEventListener('click',deleteTodoHandler);
 
 
 
