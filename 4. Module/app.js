80f74b52879358110b8fc9599f76b175d1da890f
JoHo99 <ghdi926@naver.com>
2023-08-10 13:05:01 +0900

    Module 학습 ESlint 설정

--- 4. Module/app.js
+++ 4. Module/app.js
@@ -1,15 +1,20 @@
-import { $btn } from "./getDOM.JS";
-import ABC, { clickHandler as ch } from "./event.JS";
+/* eslint-disable linebreak-style */
 
+// eslint-disable-next-line quotes
+import { $btn} from "./getDOM.JS";
+import ABC, {clickHandler as ch} from './event.JS';
+// eslint-disable-next-line linebreak-style
 
+// eslint-disable-next-line padded-blocks
 const init = ()=>{
-    $btn.style.background = 'aqua'
-    $btn.addEventListener('click',ch);
+  // eslint-disable-next-line no-trailing-spaces
+    
+  // eslint-disable-next-line indent
+    $btn.style.background = 'aqua';
+  $btn.addEventListener('click', ch);
 
-    const dog = new ABC('초코',3);
-    dog.init();
+  const dog = new ABC('초코', 3);
+  dog.info();
 };
 
 init();
-
-
--- 4. Module/event.js
+++ 4. Module/event.js
@@ -1,19 +1,20 @@
+
 // getDOM에서 가져온 것
 import { $p as $para } from "./getDOM.JS";
 
 // event에서 만든 것
 const $p = document.querySelector('p');
 
-export const clickHandler = e =>{
+export const clickHandler = e => {
     $para.textContent = '하이';
 }
 
 export default class {
-    constructor(name,age){
+    constructor(name, age) {
         this.name = name;
-        this.age =age;
+        this.age = age;
     }
-    instanceof(){
+    instanceof() {
         alert(`${this.name} : 저는 ${this.age}살 입니다`)
     }
 }
